cd5ea14804ce5dbed6fc4425faa29307e6592b6d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-04-29 18:59:08 +0200

    Quickfixes WP Coding Standards
    - Strict comparison
    - Yoda notation
    - Spacing/indention

--- includes/widget-css-classes-library.class.php
+++ includes/widget-css-classes-library.class.php
@@ -25,10 +25,12 @@ class WCSSC_Lib {
 	 */
 	public static function add_settings_link( $links, $file ) {
 		static $this_plugin;
-		if ( !$this_plugin ) $this_plugin = WCSSC_BASENAME;
+		if ( ! $this_plugin ) {
+			$this_plugin = WCSSC_BASENAME;
+		}
 
-		if ( $file == $this_plugin ) {
-			$settings_link = '<a href="'.admin_url( 'options-general.php?page=widget-css-classes-settings' ).'">'.esc_attr__( 'Settings', 'widget-css-classes' ).'</a>';
+		if ( $file === $this_plugin ) {
+			$settings_link = '<a href="' . admin_url( 'options-general.php?page=widget-css-classes-settings' ) . '">' . esc_attr__( 'Settings', 'widget-css-classes' ) . '</a>';
 			array_unshift( $links, $settings_link );
 		}
 
@@ -42,8 +44,8 @@ class WCSSC_Lib {
 	 */
 	public static function admin_footer() {
 		$plugin_data = get_plugin_data( WCSSC_FILE );
-		echo $plugin_data['Title'].' | '.esc_attr__( 'Version', 'widget-css-classes' ).' '.esc_html( $plugin_data['Version'] ).' | '.$plugin_data['Author'].
-			' | <a href="http://codebrainmedia.com">CodeBrain Media</a> | <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=cindy@cleverness.org">'.esc_attr__( 'Donate', 'widget-css-classes' ).'</a>
+		echo $plugin_data['Title'] . ' | ' . esc_attr__( 'Version', 'widget-css-classes' ) . ' ' . esc_html( $plugin_data['Version'] ) . ' | ' . $plugin_data['Author'] .
+			' | <a href="http://codebrainmedia.com">CodeBrain Media</a> | <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=cindy@cleverness.org">' . esc_attr__( 'Donate', 'widget-css-classes' ) . '</a>
 		<br />';
 	}
 
@@ -62,7 +64,7 @@ class WCSSC_Lib {
 		}
 
 		// check if the db version is the same as the db version constant
-		if ( $installed_version != WCSSC_DB_VERSION ) {
+		if ( (int) WCSSC_DB_VERSION !== (int) $installed_version ) {
 			// update options
 			self::set_options( $installed_version );
 			update_option( 'WCSSC_db_version', WCSSC_DB_VERSION );
@@ -78,15 +80,15 @@ class WCSSC_Lib {
 	 */
 	public static function set_options( $version ) {
 
-		if ( $version == 0 ) {
+		if ( 0 === (int) $version ) {
 			// add default options
 			$options = array(
-				'show_id'		=> 0,
-				'type'			=> 1,
-				'defined_classes'	=> '',
-				'show_number'		=> 1,
-				'show_location'		=> 1,
-				'show_evenodd'		=> 1,
+				'show_id'         => 0,
+				'type'            => 1,
+				'defined_classes' => '',
+				'show_number'     => 1,
+				'show_location'   => 1,
+				'show_evenodd'    => 1,
 			);
 
 			add_option( 'WCSSC_options', $options );
@@ -96,15 +98,15 @@ class WCSSC_Lib {
 
 			if ( $version < 1.2 ) {
 				$general_options         = get_option( 'WCSSC_options' );
-				$general_options['show_number'] = 1;
+				$general_options['show_number']   = 1;
 				$general_options['show_location'] = 1;
-				$general_options['show_evenodd'] = 1;
+				$general_options['show_evenodd']  = 1;
 				update_option( 'WCSSC_options', $general_options );
 			}
 			if ( $version < 1.3 ) {
-				$general_options         = get_option( 'WCSSC_options' );
+				$general_options = get_option( 'WCSSC_options' );
 				// Hide option is now 0 instead of 3
-				if ( isset( $general_options['type'] ) && $general_options['type'] == 3 ) {
+				if ( isset( $general_options['type'] ) && 3 === (int) $general_options['type'] ) {
 					$general_options['type'] = 0;
 				}
 				// dropdown settings are renamed to defined_classes
@@ -115,7 +117,7 @@ class WCSSC_Lib {
 				unset( $general_options['dropdown'] );
 				update_option( 'WCSSC_options', $general_options );
 			}
-		}
+		} // End if().
 	}
 
 }
