ccc0c26ba541373649facc788bccafe7305f6d1b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-06-20 13:44:57 +0200

    Add setting storage unit tests

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * Widget CSS Classes - Unit tests
+ *
+ * @author Jory Hogeveen <info@keraweb.nl>
+ * @package widget-css-classes
+ * @version 1.4.1
  */
 
 class WCSSC_UnitTest extends WP_UnitTestCase {
@@ -11,5 +15,38 @@ class WCSSC_UnitTest extends WP_UnitTestCase {
 	function test_activated() {
 
 		$this->assertTrue( is_plugin_active( TEST_WCSSC_PLUGIN_PATH ) );
+
+		widget_css_classes_loader();
+	}
+
+	/**
+	 * Test setting storage functionality.
+	 */
+	function test_settings() {
+
+		$settings = array(
+			'non_existing_key'  => 'test',
+			'fix_widget_params' => 0,
+			'show_id'           => 1,
+			'type'              => 3,
+			'defined_classes'   => 'test;semicolon space,comma',
+			'show_number'       => 1,
+			'show_location'     => 0,
+			'show_evenodd'      => 1,
+		);
+
+		WCSSC_Lib::update_settings( $settings );
+
+		$compare = array(
+			'fix_widget_params' => 0,
+			'show_id'           => 1,
+			'type'              => 3,
+			'defined_classes'   => array( 'test', 'semicolon', 'space', 'comma' ),
+			'show_number'       => 1,
+			'show_location'     => 0,
+			'show_evenodd'      => 1,
+		);
+
+		$this->assertEquals( $compare, WCSSC_Lib::get_settings() );
 	}
 }
