525f1b4580e57a191b92b67fdf778e4dbacaba18
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-06-22 09:25:15 +0200

    Remove relCopy dependency + update changelog

--- js/widget-css-classes.js
+++ js/widget-css-classes.js
@@ -18,15 +18,22 @@ jQuery( document ).ready( function ( $ ) {
 		}
 	} ).filter(':checked').trigger('change');
 
-	$( '.wcssc_copy' ).relCopy( {} );
-
-	$( 'p' ).on( 'click', '.wcssc_remove', function(e) {
+	// Defined classes new and delete handlers.
+	$( document ).on( 'click', '.wcssc_defined_classes.wcssc_sort_fixed .wcssc_copy', function(e) {
 		e.preventDefault();
-		$( this ).parent().slideUp( function () {
+		var elem = $( this ).parents('.wcssc_defined_classes');
+		var clone = elem.clone().hide().insertBefore( elem );
+		clone.removeClass('wcssc_sort_fixed').find('.wcssc_copy').remove();
+		clone.slideDown('fast');
+
+	} ).on( 'click', '.wcssc_defined_classes .wcssc_remove', function(e) {
+		e.preventDefault();
+		$( this ).parent().slideUp( 'fast', function () {
 			$( this ).remove();
 		} );
 	} );
 
+	// Make defined classes sortable.
 	if ( $.isFunction( $.fn.sortable ) ) {
 		$('.wcssc_sortable .wcssc_sort').show();
 		$('.wcssc_sortable').sortable({
