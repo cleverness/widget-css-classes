ec10254a93a8da507928cb5d3ed1d86ce1977907
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-01-02 16:14:05 +0100

    Compatibility with dFactory Responsive Lightbox widget option
    Checks for the original callback

--- includes/widget-css-classes.class.php
+++ includes/widget-css-classes.class.php
@@ -700,16 +700,14 @@ class WCSSC {
 		$widget_opt = null;
 
 		$active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
-		// If Widget Logic plugin is enabled, use it's callback
+		// If Widget Logic plugin is enabled, use it's callback.
 		if ( in_array( 'widget-logic/widget_logic.php', $active_plugins, true ) ) {
 			$widget_logic_options = get_option( 'widget_logic' );
 			if ( ! empty( $widget_logic_options['widget_logic-options-filter'] ) ) {
 				$widget_opt = get_option( $widget_obj['callback_wl_redirect'][0]->option_name );
-			} else {
-				$widget_opt = get_option( $widget_obj['callback'][0]->option_name );
 			}
 		}
-		// If Widget Context plugin is enabled, use it's callback
+		// If Widget Context plugin is enabled, use it's callback.
 		elseif ( in_array( 'widget-context/widget-context.php', $active_plugins, true ) ) {
 			$callback = isset( $widget_obj['callback_original_wc'] ) ? $widget_obj['callback_original_wc'] : null;
 			$callback = ! $callback && isset( $widget_obj['callback'] ) ? $widget_obj['callback'] : null;
@@ -718,24 +716,34 @@ class WCSSC {
 				$widget_opt = get_option( $callback[0]->option_name );
 			}
 		}
-		// If Widget Output filter is enabled (f.e. by WP External Links plugin), don't use it's callback but the original callback
+		// If Widget Output filter is enabled (f.e. by WP External Links plugin), don't use it's callback but the original callback.
 		elseif ( isset( $widget_obj['_wo_original_callback'] ) ) {
 			$widget_opt = get_option( $widget_obj['_wo_original_callback'][0]->option_name );
 		}
 
-		// Default callback
-		else {
-			// Check if WP Page Widget is in use
+		// Default callback.
+		if ( null === $widget_opt ) {
+			// Check if WP Page Widget is in use.
 			global $post;
 			$id = ( isset( $post->ID ) ? get_the_ID() : null );
 			if ( isset( $id ) && get_post_meta( $id, '_customize_sidebars' ) ) {
 				$custom_sidebarcheck = get_post_meta( $id, '_customize_sidebars' );
 			}
+
+			$option_name = '';
+			if ( isset( $widget_obj['callback'][0]->option_name ) ) {
+				$option_name = $widget_obj['callback'][0]->option_name;
+			} elseif ( isset( $widget_obj['original_callback'][0]->option_name ) ) {
+				// @since  1.5.3  Compatibility with dFactory Responsive Lightbox plugin.
+				$option_name = $widget_obj['original_callback'][0]->option_name;
+			}
+
 			if ( isset( $custom_sidebarcheck[0] ) && ( 'yes' === $custom_sidebarcheck[0] ) ) {
-				$widget_opt = get_option( 'widget_' . $id . '_' . substr( $widget_obj['callback'][0]->option_name, 7 ) );
-			} elseif ( isset( $widget_obj['callback'][0]->option_name ) ) {
-				// Default
-				$widget_opt = get_option( $widget_obj['callback'][0]->option_name );
+				$widget_opt = get_option( 'widget_' . $id . '_' . substr( $option_name, 7 ) );
+			}
+			elseif ( $option_name ) {
+				// Default.
+				$widget_opt = get_option( $option_name );
 			}
 		}
 
